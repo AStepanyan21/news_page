4703c5a281455809d3ad98a985c244e580c3e965
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2021-02-22 19:10:20 +0400

    init heroku

--- news_page/settings.py
+++ news_page/settings.py
@@ -85,6 +85,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
